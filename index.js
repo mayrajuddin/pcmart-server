a12abeb9741d5681bdeae8c2f69776c8ef9c322b
uddinmayraj <mayrajuddin03@gmail.com>
2022-11-25 12:38:14 +0600

    connect mongodb and set env

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+require('dotenv').config()
 const port = process.env.PORT || 8000
 const app = express()
 
@@ -11,5 +15,15 @@ app.get('/', async (req, res) => {
     res.send('pcmart server is running')
 })
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oejruqx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally { }
+}
+run().catch(err => console.log(err))
 
 app.listen(port, () => console.log(`pcmart runing on ${port}`))
